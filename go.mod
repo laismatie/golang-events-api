5f02ff4d313a9cce87ddb8b9f688de8082828c88
Laís Matie <lmatiehara@gmail.com>
2024-06-20 21:16:16 -0400

    [NEW] first commit, start create event domain

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/laismatie/golang-events-api
+
+go 1.22.4
---
+++ internal/events/domain/event.go
@@ -0,0 +1,59 @@
+package domain
+
+import "time"
+
+type Rating string
+
+const (
+	RatingLivre Rating = "L"
+	Rating10    Rating = "L10"
+	Rating12    Rating = "L12"
+	Rating14    Rating = "L14"
+	Rating16    Rating = "L16"
+	Rating18    Rating = "L18"
+)
+
+type SpotStatus string
+
+const (
+	SpotStatusAvailable SpotStatus = "available"
+	SpotStatusSold      SpotStatus = "sold"
+)
+
+type Spot struct {
+	ID       string
+	EventID  string
+	Name     string
+	Status   SpotStatus
+	TicketID string
+}
+
+type TicketType string
+
+const (
+	TicketTypeHalf TicketType = "half"
+	TicketTypeFull TicketType = "full"
+)
+
+type Ticket struct {
+	ID         string
+	EventID    string
+	Spot       *Spot
+	TicketType TicketType
+	Price      float64
+}
+
+type Event struct {
+	ID           string
+	Name         string
+	Location     string
+	Organization string
+	Rating       string
+	Date         time.Time
+	ImageURL     string
+	Capacity     int
+	Price        float64
+	PartnerID    int
+	Spots        []Spot
+	Ticket       []Ticket
+}
